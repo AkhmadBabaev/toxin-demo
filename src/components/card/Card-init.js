b676a06ec530a07ba551b278a2093f36b2a44efb
AkhmadBabaev <akhmad.babaev@gmail.com>
2020-06-03 15:06:22 +0300

    Get rid of useless Object.keys #6

--- src/components/card/Card-init.js
+++ src/components/card/Card-init.js
@@ -2,5 +2,5 @@ import Card from './Card';
 
 window.addEventListener('load', () => {
   const cards = document.querySelectorAll('.js-card');
-  Object.keys(cards).forEach((_, index) => new Card(cards[index]));
+  cards.forEach((_, index) => new Card(cards[index]));
 });
--- src/components/star-rating/StarRating-init.js
+++ src/components/star-rating/StarRating-init.js
@@ -1,4 +1,4 @@
 import StarRating from './StarRating';
 
 const starRatingList = document.querySelectorAll('.js-star-rating');
-Object.keys(starRatingList).forEach((_, index) => new StarRating(starRatingList[index]));
+starRatingList.forEach((_, index) => new StarRating(starRatingList[index]));
